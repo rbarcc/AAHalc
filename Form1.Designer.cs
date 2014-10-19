35313f290fd81ebe0274abb73c8e405e2b317adf
rbarcc <rbarcc@gmail.com>
2014-10-19 17:04:45 +0200

    Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace AAHalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Phase = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.HoursInput = new System.Windows.Forms.NumericUpDown();
+            this.MinutesInput = new System.Windows.Forms.NumericUpDown();
+            this.TimeLabel = new System.Windows.Forms.Label();
+            this.HalcTimes = new System.Windows.Forms.ListBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.HoursInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MinutesInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hours:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(220, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Phase";
+            // 
+            // Phase
+            // 
+            this.Phase.FormattingEnabled = true;
+            this.Phase.Items.AddRange(new object[] {
+            "At Peace",
+            "At War",
+            "Conflict"});
+            this.Phase.Location = new System.Drawing.Point(263, 12);
+            this.Phase.Name = "Phase";
+            this.Phase.Size = new System.Drawing.Size(71, 21);
+            this.Phase.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(108, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Minutes:";
+            // 
+            // HoursInput
+            // 
+            this.HoursInput.Location = new System.Drawing.Point(51, 13);
+            this.HoursInput.Name = "HoursInput";
+            this.HoursInput.Size = new System.Drawing.Size(51, 20);
+            this.HoursInput.TabIndex = 7;
+            // 
+            // MinutesInput
+            // 
+            this.MinutesInput.Location = new System.Drawing.Point(161, 13);
+            this.MinutesInput.Name = "MinutesInput";
+            this.MinutesInput.Size = new System.Drawing.Size(53, 20);
+            this.MinutesInput.TabIndex = 8;
+            // 
+            // TimeLabel
+            // 
+            this.TimeLabel.AutoSize = true;
+            this.TimeLabel.Font = new System.Drawing.Font("Arial", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.TimeLabel.Location = new System.Drawing.Point(5, 39);
+            this.TimeLabel.Name = "TimeLabel";
+            this.TimeLabel.Size = new System.Drawing.Size(229, 60);
+            this.TimeLabel.TabIndex = 9;
+            this.TimeLabel.Text = "00:00:00";
+            this.TimeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // HalcTimes
+            // 
+            this.HalcTimes.FormattingEnabled = true;
+            this.HalcTimes.Location = new System.Drawing.Point(12, 102);
+            this.HalcTimes.Name = "HalcTimes";
+            this.HalcTimes.Size = new System.Drawing.Size(283, 147);
+            this.HalcTimes.TabIndex = 10;
+            this.HalcTimes.SelectedIndexChanged += new System.EventHandler(this.HalcTimes_SelectedIndexChanged);
+            // 
+            // Start
+            // 
+            this.Start.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Start.BackgroundImage")));
+            this.Start.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Start.Location = new System.Drawing.Point(240, 39);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(94, 39);
+            this.Start.TabIndex = 12;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(301, 84);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(37, 31);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(301, 121);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(37, 31);
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(301, 158);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(37, 31);
+            this.pictureBox3.TabIndex = 15;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(301, 195);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(37, 31);
+            this.pictureBox4.TabIndex = 16;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(301, 232);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(37, 31);
+            this.pictureBox5.TabIndex = 17;
+            this.pictureBox5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(342, 263);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.HalcTimes);
+            this.Controls.Add(this.TimeLabel);
+            this.Controls.Add(this.MinutesInput);
+            this.Controls.Add(this.HoursInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Phase);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "H A L C Y O N A B O Y S";
+            ((System.ComponentModel.ISupportInitialize)(this.HoursInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MinutesInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox Phase;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown HoursInput;
+        private System.Windows.Forms.NumericUpDown MinutesInput;
+        private System.Windows.Forms.Label TimeLabel;
+        private System.Windows.Forms.ListBox HalcTimes;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AAHalc
+{
+    public partial class Form1 : Form
+    {
+        private delegate void UpdateDelegate();
+        private readonly Object _thisLock = new Object();
+        private readonly Image _bibleThump;
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateDelegate up = TimerTick;
+            TimeLeft = 1000;
+            Thread thread1 = new Thread(new ThreadStart(up));
+            thread1.Start();
+            Phase.SelectedIndex = 0;
+            _bibleThump = Start.BackgroundImage;
+            Start.BackgroundImage = null;
+        }
+
+        public decimal TimeLeft = 0;
+        public bool CountdownEnabled = false;
+        void TimerTick()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                if(!CountdownEnabled) continue;
+                lock (_thisLock)
+                {
+                    SetTimer(SecondsToString((int)TimeLeft));
+                    TimeLeft--;
+                }
+            }
+        }
+
+        string SecondsToString(int value)
+        {
+            int numberOfSeconds = value%60;
+            int numberOfMinutes = (value / 60) % 60;
+            int numberOfHours = (value / 3600) % 3600;
+            return numberOfHours.ToString("0") + ":" + numberOfMinutes.ToString("0") + ":" + numberOfSeconds;
+        }
+
+        delegate void SetTextCallback(string text);
+        private void SetTimer(string s)
+        {
+            if (TimeLabel.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetTimer);
+                Invoke(d, new object[] {s});
+            }
+            else
+            {
+                TimeLabel.Text = s;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        // halcyona starts, at war for 1h 30minutes
+        // 4 hours at peace
+        // 15 minutes conflict
+        // total: 5 hours 45 minutes
+        private const int HalcyonaWarHours = 1;
+        private const int HalcyonaWarMinutes = 30;
+        private const int HalcyonaPeaceHours = 4;
+        private const int HalcyonaPeaceMinutes = 0;
+        private const int HalcyonaConflictMinutes = 15;
+        const int HoursBetweenHalcyonas = HalcyonaWarHours + HalcyonaPeaceHours;
+        const int MinutesBetweenHalcyonas = HalcyonaWarMinutes + HalcyonaPeaceMinutes + HalcyonaConflictMinutes;
+        private void Start_Click(object sender, EventArgs e)
+        {
+            CountdownEnabled = true;
+            DateTime today = DateTime.Now;
+            int hoursLeft = (int)HoursInput.Value;
+            int minutesLeft = (int)MinutesInput.Value;
+            
+            Console.WriteLine(Phase.SelectedItem.ToString());
+            switch (Phase.SelectedItem.ToString())
+            {
+                case "At War":
+                    hoursLeft += HalcyonaPeaceHours;
+                    minutesLeft += HalcyonaPeaceMinutes + HalcyonaConflictMinutes;
+                    break;
+                case "At Peace":
+                    minutesLeft += HalcyonaConflictMinutes;
+                    break;
+                case "Conflict":
+                    break;
+                default:
+                    throw new ArgumentException("Phase is wrong");
+            }
+
+            lock (_thisLock)
+            {
+                TimeLeft = 3600 * hoursLeft + 60 * minutesLeft;
+                HalcTimes.Items.Clear();
+                for (int i = 0; i < 8; i++)
+                {
+                    DateTime tmp = today;
+                    Console.WriteLine(tmp.TimeOfDay);
+                    tmp = tmp.AddHours(hoursLeft + i * (HoursBetweenHalcyonas));
+                    tmp = tmp.AddMinutes(minutesLeft + i * (MinutesBetweenHalcyonas));
+                    string s = string.Format("{0:hh:mm:ss t}", tmp);
+                    HalcTimes.Items.Add(tmp.ToShortTimeString());
+                }
+                Start.BackgroundImage = _bibleThump;
+            }
+        }
+
+        private void HalcTimes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
